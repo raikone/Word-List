8000af716f86009e4b8c298ab7b4de6353222130
raikone <raikone@gmail.com>
2020-07-04 22:51:47 -0400

    1 file modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { WordsComponent } from './words/words.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: WordsComponent },
+    ]) ],
+  declarations: [ AppComponent, WordsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
