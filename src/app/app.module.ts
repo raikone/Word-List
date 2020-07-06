18022057d4490309c56ba9a241425a1b6630c55a
raikone <raikone@gmail.com>
2020-07-06 09:36:12 -0400

    9 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WordsComponent } from './words/words.component';
+//import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
-    FormsModule,
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: WordsComponent },
-    ]) ],
-  declarations: [ AppComponent, WordsComponent ],
-  bootstrap:    [ AppComponent ]
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    //MatCardModule,
+    WordsComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { level1 } from '../word-list';
+
 @Component({
   selector: 'app-words',
   templateUrl: './words.component.html',
   styleUrls: ['./words.component.css']
 })
-export class WordsComponent implements OnInit {
+export class WordsComponent{
+  clase = ['capitalize', 'upper', 'lower']
+  font = ['ubuntu', 'anton', 'yellowtail', 'dancing']
 
-  constructor() { }
+  words = level1;
+  
+  item = level1[Math.floor(Math.random() * level1.length)];
+  fuente = this.font[Math.floor(Math.random() * this.font.length)];
+  tipo = this.clase[Math.floor(Math.random() * this.clase.length)];
 
-  ngOnInit() {
+  change() {
+    this.item = level1[Math.floor(Math.random() * level1.length)];
+    this.fuente = this.font[Math.floor(Math.random() * this.font.length)];
+    this.tipo = this.clase[Math.floor(Math.random() * this.clase.length)];
   }
 
 }
